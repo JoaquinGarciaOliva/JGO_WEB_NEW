13c7b897b28cb446a4d5196f1c86ef883f6af938
JoaquinGarciaOliva <jgo.matematicas@gmail.com>
2026-08-12 17:06:35 +0200

    Add files via upload

--- Arboles/Primaria.js
+++ Arboles/Primaria.js
@@ -7,7 +7,7 @@
   <div class="folder"
        id="1."
        nivel="1"
-       imagen="Imagenes/Primaria/hoja_green.png"
+       imagen="Imagenes/Primaria/hoja_verdeoscuro.png"
        imagen2="Imagenes/"
        Tipo=""
        Youtube="Temario/Matematicas/Pagina_video.html?videoId=01_Números Naturales/1.1.2.1.Naturales_Suma_Video.mp4"
@@ -17,7 +17,7 @@
        interactivo="Temario/Interactivos/1.1.2.1.Naturales_Suma/3_sumandos/suma3_aprender.html"
        onclick="toggleVisibility2(this,'1.')">
 
-    <img  class="manImg" src="Imagenes/Primaria/hoja_green.png">
+    <img  class="manImg" src="Imagenes/Primaria/hoja_verdeoscuro.png">
     <span style="display: none;">1.</span>
     <span class="Arbol_Nombre_2">Suma</span>
   </div></li>
@@ -61,7 +61,7 @@
   <div class="tree-node"
        id="3.1."
        nivel="2"
-       imagen="Imagenes/Primaria/hoja_yellow.png"
+       imagen="Imagenes/Primaria/hoja.png"
        imagen2="Imagenes/"
        Tipo=""
        Youtube="Temario/Matematicas/Pagina_video.html?videoId=01_Números Naturales/1.1.2.3.1.Naturales_Multiplicar_Video.mp4"
@@ -71,7 +71,7 @@
        interactivo="Temario/Interactivos/1.1.2.3.1.Naturales_Multiplicar/Multiplicar_aprender.html"
        onclick="toggleVisibility2(this,'3.1.')">
 
-    <img  class="manImg" src="Imagenes/Primaria/hoja_yellow.png">
+    <img  class="manImg" src="Imagenes/Primaria/hoja.png">
     <span style="display: none;">3.1.</span>
     <span class="Arbol_Nombre_2">Genérica.</span>
   </div></li>
@@ -79,7 +79,7 @@
   <div class="tree-node"
        id="3.2."
        nivel="2"
-       imagen="Imagenes/Primaria/hoja_red.png"
+       imagen="Imagenes/Primaria/hoja_yellow.png"
        imagen2="Imagenes/"
        Tipo=""
        Youtube="Temario/Matematicas/Pagina_video.html?videoId=01_Números Naturales/1.1.2.3.2.Multiplicar_Linea_Video.mp4"
@@ -89,7 +89,7 @@
        interactivo="Temario/Interactivos/1.1.2.3.2.Naturales_Multiplicar_Linea/Multiplicar_Linea_aprender.html"
        onclick="toggleVisibility2(this,'3.2.')">
 
-    <img  class="manImg" src="Imagenes/Primaria/hoja_red.png">
+    <img  class="manImg" src="Imagenes/Primaria/hoja_yellow.png">
     <span style="display: none;">3.2.</span>
     <span class="Arbol_Nombre_2">En línea.</span>
   </div></li>
@@ -97,7 +97,7 @@
   <div class="tree-node"
        id="3.3."
        nivel="2"
-       imagen="Imagenes/Primaria/hoja.png"
+       imagen="Imagenes/Primaria/hoja_red.png"
        imagen2="Imagenes/"
        Tipo=""
        Youtube="Temario/Matematicas/Pagina_video.html?videoId=01_Números Naturales/1.1.2.3.3.Multiplicar_Potencias10_Video.mp4"
@@ -107,7 +107,7 @@
        interactivo="Temario/Interactivos/1.1.2.3.3.Naturales_Multiplicar_10/Multiplicar_Potencias_10.html"
        onclick="toggleVisibility2(this,'3.3.')">
 
-    <img  class="manImg" src="Imagenes/Primaria/hoja.png">
+    <img  class="manImg" src="Imagenes/Primaria/hoja_red.png">
     <span style="display: none;">3.3.</span>
     <span class="Arbol_Nombre_2">Por Potencias de 10.</span>
   </div></li></ul>
@@ -115,17 +115,17 @@
   <div class="folder"
        id="4."
        nivel="1"
-       imagen="Imagenes/Primaria/hoja.png"
+       imagen="Imagenes/Primaria/hoja_green.png"
        imagen2="Imagenes/"
        Tipo=""
-       Youtube=""
-       teoria=""
-       ejemplos=""
-       ejercicios=""
+       Youtube="Temario/Matematicas/Pagina_video.html?videoId=01_Números Naturales/1.1.2.4.Naturales_Division_Video.mp4"
+       teoria="Temario/Matematicas/01_Números Naturales/1.1.2.4.Naturales_Division_Teoria.html"
+       ejemplos="Temario/Matematicas/01_Números Naturales/1.1.2.4.Naturales_Division_Ejemplos.html"
+       ejercicios="Temario/Matematicas/01_Números Naturales/1.1.2.4.Naturales_Division_Ejercicios.html"
        interactivo="Temario/Interactivos/1.1.2.4.Naturales_Dividir/Dividir_Aprender_1.html"
        onclick="toggleVisibility2(this,'4.')">
 
-    <img  class="manImg" src="Imagenes/Primaria/hoja.png">
+    <img  class="manImg" src="Imagenes/Primaria/hoja_green.png">
     <span style="display: none;">4.</span>
     <span class="Arbol_Nombre_2">División. Prueba de la división.</span>
   </div></li></ul>`; 
